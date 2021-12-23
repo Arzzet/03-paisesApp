cba695174daca593357d8964929983222fe588d2
Iwo <iwoskater3@hotmail.com>
2021-12-23 12:10:01 +0100

    working api call for countries, search and table components

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -6,11 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent  {
 
-  constructor() { }
+  termino: string = 'Hola';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  ngOnInit(): void {
+  constructor(private paisService: PaisService) { }
+
+  buscar(termino: string){
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais(termino)
+    .subscribe( (paises) => {
+      console.log(paises);
+      this.paises = paises;
+
+    }, (err) => {
+      this.hayError = true;
+      this.paises = [];
+      
+      });
+
+
   }
+ 
 
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { AppRoutingModule } from '../../app-routing.module';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: [
+    `
+    li{cursor:pointer;}
+    li:hover{background-color: #ABCDEF;}
+    `
   ]
 })
 export class SidebarComponent implements OnInit {
