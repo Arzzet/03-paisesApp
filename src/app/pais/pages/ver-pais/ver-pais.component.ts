5048611aa9faad5d1467ff720096217616657235
Iwo <iwoskater3@hotmail.com>
2021-12-25 11:37:45 +0100

    get country info from url, using switchmap - rxjs

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+    ) { }
 
   ngOnInit(): void {
+    // METODO USANDO SWITCHMAP DE RXJS ( TRANSFORMA UN OBSERVABLE EN OTRO OBSERVABLE )
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.paisService.getPaisPorCodigo(id))
+      )
+      .subscribe( resp => {
+        console.log(resp);
+      })
+
+    // METODO TRADICIONAL DE OBTENER EL ID DEL PAIS ( USANDO 2 SUBSCRIBES )
+    // this.activatedRoute.params
+    // .subscribe( params =>{
+    //   console.log(params.id);
+
+    //   this.paisService.getPaisPorCodigo(params.id)
+    //   .subscribe( pais =>{
+    //     console.log(pais);
+    //   })
+
+    // })
+
   }
 
 }
